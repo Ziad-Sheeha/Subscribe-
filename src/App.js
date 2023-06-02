e08c4d7b7ff6a41624182f80d98a9bf92bfde770
Hassan Sheeha <112267509+HassanSheeha@users.noreply.github.com>
2023-06-02 19:25:20 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import Window from './components/Window';
-import SuccessWindow from './components/SuccessWindow';
-import { Routes, Route } from 'react-router-dom';
+import Window from "./components/Window";
+import SuccessWindow from "./components/SuccessWindow";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        {/* <Routes>
-          <Route
-            path="/"
-            element={<Window />}
-          />
-          <Route
-            path="success"
-            element={<SuccessWindow />}
-          />
-        </Routes> */}
-        <Window />
-        {/* <SuccessWindow /> */}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Window />} />
+					<Route path="success" element={<SuccessWindow />} />
+				</Routes>
+				{/* <Window /> */}
+				{/* <SuccessWindow /> */}
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,82 +1,84 @@
-import React, { useState } from 'react';
-import SuccessWindow from './SuccessWindow';
+import React, { useState } from "react";
+import SuccessWindow from "./SuccessWindow";
+import { useNavigate } from "react-router-dom";
 
 const SubscribeForm = () => {
-  const [mailInput, setMailInput] = useState('');
+	const [mailInput, setMailInput] = useState("");
+	let navigate = useNavigate();
 
-  const [isincludeAt, setIsincludeAt] = useState(true);
-  const [isValidentity, setIsValidentity] = useState(true);
+	const [isincludeAt, setIsincludeAt] = useState(true);
+	const [isValidentity, setIsValidentity] = useState(true);
 
-  const inputHandler = (e) => {
-    setMailInput(e.target.value);
-    setIsincludeAt(true);
-    setIsValidentity(true);
-  };
+	const inputHandler = (e) => {
+		setMailInput(e.target.value);
+		setIsincludeAt(true);
+		setIsValidentity(true);
+	};
 
-  const formHandler = (e) => {
-    e.preventDefault();
-    if (!mailInput.endsWith('.com') && !mailInput.includes('@')) {
-      setIsValidentity(false);
-      setIsincludeAt(false);
-      return;
-    }
-    if (!mailInput.includes('@')) {
-      setIsincludeAt(false);
-      return;
-    }
-    if (!mailInput.endsWith('.com')) {
-      setIsValidentity(false);
-      return;
-    }
-    console.log('Email Submitted!');
-    setMailInput('');
-  };
+	const goToSuccess = () => {
+		navigate("/success", { state: { email: mailInput } });
+	};
 
-  const inputHandlerOnBlur = (e) => {
-    if (!mailInput.includes('@')) {
-      setIsincludeAt(false);
-    }
-    if (!mailInput.endsWith('.com')) {
-      setIsValidentity(false);
-    }
-  };
+	const formHandler = (e) => {
+		e.preventDefault();
+		if (!mailInput.endsWith(".com") && !mailInput.includes("@")) {
+			setIsValidentity(false);
+			setIsincludeAt(false);
+			return;
+		}
+		if (!mailInput.includes("@")) {
+			setIsincludeAt(false);
+			return;
+		}
+		if (!mailInput.endsWith(".com")) {
+			setIsValidentity(false);
+			return;
+		}
+		setMailInput("");
+		goToSuccess();
+	};
 
-  return (
-    <form onSubmit={formHandler}>
-      <div className="error">
-        <label>Email address</label>
-      </div>
-      <input
-        value={mailInput}
-        onChange={inputHandler}
-        onBlur={inputHandlerOnBlur}
-        placeholder="email@company.com"
-      />
-      {!isincludeAt && isValidentity && (
-        <span className="error-message">your email must has an ' @ '</span>
-      )}
-      {!isValidentity && isincludeAt && (
-        <span className="error-message">
-          your email must ends with '
-          <span className="bold text-colored"> .com </span>'
-        </span>
-      )}
-      {!isValidentity && !isincludeAt && (
-        <span className="error-message">
-          your email must has an '{' '}
-          <span className="bold text-colored"> @ </span> and ends with
-          <span className="bold text-colored"> .com </span> '
-        </span>
-      )}
-      <button
-        to={'/success'}
-        className="btn"
-        type="submit"
-      >
-        Subscribe to monthly newsteller
-      </button>
-    </form>
-  );
+	const inputHandlerOnBlur = (e) => {
+		if (!mailInput.includes("@")) {
+			setIsincludeAt(false);
+		}
+		if (!mailInput.endsWith(".com")) {
+			setIsValidentity(false);
+		}
+	};
+
+	return (
+		<form onSubmit={formHandler}>
+			<div className="error">
+				<label>Email address</label>
+			</div>
+			<input
+				value={mailInput}
+				onChange={inputHandler}
+				onBlur={inputHandlerOnBlur}
+				placeholder="email@company.com"
+			/>
+			{!isincludeAt && isValidentity && (
+				<span className="error-message">your email must has an ' @ '</span>
+			)}
+			{!isValidentity && isincludeAt && (
+				<span className="error-message">
+					your email must ends with '
+					<span className="bold text-colored"> .com </span>'
+				</span>
+			)}
+			{!isValidentity && !isincludeAt && (
+				<span className="error-message">
+					your email must has an '{" "}
+					<span className="bold text-colored"> @ </span> and ends with
+					<span className="bold text-colored"> .com </span> '
+				</span>
+			)}
+			<button className="btn" type="submit">
+				Subscribe to monthly newsteller
+			</button>
+		</form>
+	);
 };
 
 export default SubscribeForm;
